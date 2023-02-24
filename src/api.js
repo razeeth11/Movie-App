95af730826e2c4bd7cd38dbfcbce0be3e4f6af42
razeeth11 <razeeth011@gmail.com>
2023-02-24 21:12:54 +0530

    vercel

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
 // export const API = "https://63db75e9a3ac95cec5a22b2d.mockapi.io/nutflix"
-// export const API = "https://localhost:4150"
-export const API = "http://localhost:4150"
+export const API = "https://node-crud-app-for-movies-omega.vercel.app"
+// export const API = "http://localhost:4150"
